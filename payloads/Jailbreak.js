83afbbd3020f0bd15c3c7e666eeb5104faa0d67e
speed2games <speed20games@gmail.com>
2025-07-21 22:07:02 +0300

    Add files via upload

--- payloads/Jailbreak.js
+++ payloads/Jailbreak.js
@@ -1,19 +1,9 @@
 export function GoldHEN() {
     window.payload_path = './payloads/GoldHEN/GoldHEN.bin';
-    setTimeout(() => {
-        sessionStorage.setItem('jbsuccess', 1);
-        document.getElementById('jailbreak').style.display = 'flex';
-        document.getElementById('loader').style.display = 'none';
-        window.location.reload();
-    }, 3000); // 3 seconds delay
+    // timeout is moved to lapse.mjs to execute after exploit success
 }
 
-export function HEN(ps4fw) {
-    window.payload_path = `./payloads/HEN/HEN-${ps4fw}.bin`;
-    setTimeout(() => {
-        sessionStorage.setItem('jbsuccess', 1);
-        document.getElementById('jailbreak').style.display = 'flex';
-        document.getElementById('loader').style.display = 'none';
-        window.location.reload();
-    }, 3000); // 3 seconds delay
+export function HEN() {
+    window.payload_path = `./payloads/HEN/HEN.bin`;
+    // timeout is moved to lapse.mjs to execute after exploit success
 }
